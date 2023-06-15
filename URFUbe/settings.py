bb8335f74daa0484a55bdb0e05cc3fa37e22e25c
simizzz <merander207203@gmail.com>
2023-06-15 08:25:14 +0500

    Change settings file

--- URFUbe/settings.py
+++ URFUbe/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -88,9 +89,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "app/staticfiles/"
+STATIC_URL = "staticfiles/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "VideoHosting/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
