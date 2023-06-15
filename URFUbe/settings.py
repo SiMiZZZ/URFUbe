bc243c45c71f95d7ba893bc04e979d589076d8af
simizzz <merander207203@gmail.com>
2023-06-15 08:07:43 +0500

    Change settings file

--- URFUbe/settings.py
+++ URFUbe/settings.py
@@ -88,7 +88,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static"
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "VideoHosting/static")]
 
